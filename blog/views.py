dedfbc6957970a00914063da271dcce900e37b15
Meee <cherrycokee@naver.com>
2021-01-07 15:10:31 +0900

    post : FBV -> CBV

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
-def index(request):
-    post = Post.objects.all()
-    context = {
-        'post' : post,
-    }
-    return render(request, 'blog/index.html', context)
+# [CBV : Class Based View] -----------------------------
+class PostList(ListView):
+    model = Post
+
+
+
+# FBV : Function Based View ------------------------------
+# def index(request):
+#     post = Post.objects.all()
+#     context = {
+#         'post' : post,
+#     }
+#     return render(request, 'blog/index.html', context)
+#
+#  -------------------------------------------------------
+
