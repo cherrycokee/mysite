7bb81f6271951d12e49071920ccf189cee114533
Meee <cherrycokee@naver.com>
2021-01-07 14:51:30 +0900

    blog post list

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
-class Post (models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
 
     created = models.DateTimeField()
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}:: {}'.format(self.title, self.author)
