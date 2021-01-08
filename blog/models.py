22288dfb86716361266a599aa4c9fffd4c0ee58b
Meee <cherrycokee@naver.com>
2021-01-08 12:12:06 +0900

    show image when head-image is none

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,9 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
 
+    # 이미지 필드 추가
+    head_image = models.ImageField(upload_to='blog/%y/%m/%d/', blank=True)
+
     created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
